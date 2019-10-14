71ccf2741dd34ab8e4ed812d1cde705cb720ee93
tommygod3 <tommygod1@live.com>
2019-10-14 19:40:53 +0000

    Finished scc lab1

--- SCC/JavaIntro/Prac1/src/myID.java
+++ SCC/JavaIntro/Prac1/src/myID.java
@@ -4,6 +4,9 @@
  */
 
 import java.io.*;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class myID
@@ -11,73 +14,102 @@ public class myID
     public String name;
     public String email;
     public String module;
+    public Scanner sUser;
 
-    public myID()
+    public myID(Scanner scanner)
     {
+        this.sUser = scanner;
         this.setDetails();
     }
 
     public void setDetails()
     {
-        this.name = myID.getInputString("Please enter your name: ");
-        this.email = myID.getInputString("Please enter your email: ");
-        this.module = myID.getInputString("Please enter your module: ");
+        this.name = this.getInputString("Please enter your name: ");
+        this.email = this.getInputString("Please enter your email: ");
+        this.module = this.getInputString("Please enter your module: ");
     }
 
-    private static String getInputString(String prompt)
+    private String getInputString(String prompt)
     {
         String result = "";
 
-        Scanner sUser = new Scanner(System.in);
         try
         {
             System.out.println(prompt);
-            result = sUser.nextLine();
+            result = this.sUser.nextLine();
         } 
-        catch(IOException e)
+        catch(NoSuchElementException e)
         {
             System.out.println("IO Exception ocurred " + e);
         }
+
         return result;
+    }
+
+    public static List<String> getLines(int number)
+    {
+        List<String> lines = new ArrayList<String>();
+        
+        try
+        {
+            Scanner sfile = new Scanner(new File("scc/JavaIntro/module.txt"));  
+            for(int i = 0; i < number; i++)
+            {
+                if(sfile.hasNext())
+                {
+                    lines.add(sfile.nextLine());
+                }
+            }
+            sfile.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        
+        return lines;
     }
 
     public static void main(String[] args)
     {
-        myID id = new myID();
-
         Scanner sUser = new Scanner(System.in);
+        myID id = new myID(sUser);
         int result;
 
         while(true)
         {
-            System.out.println("\nSelect from the following options:\n[1] print Name\n[2] print Email\n[3] print Module\n[4] quit program");
-            try
+            System.out.println("\nSelect from the following options:\n[1] print Name\n[2] print Email\n[3] print Module\n[4] print module.txt\n[5]quit program");
+            result = sUser.nextInt();
+            switch(result)
             {
-                result = sUser.nextInt();
-                switch(result)
-                {
-                    case 1:
-                        System.out.println(id.name);
-                        break;
-                    case 2:
-                        System.out.println(id.email);
-                        break;
-                    case 3:
-                        System.out.println(id.module);
-                        break;
-                    case 4:
-                        break;
-                }
-                if(result == 4)
-                {
+                case 1:
+                    System.out.println(id.name);
                     break;
-                }
+                case 2:
+                    System.out.println(id.email);
+                    break;
+                case 3:
+                    System.out.println(id.module);
+                    break;
+                case 4:
+                    System.out.println("How many lines?\n");
+                    result = sUser.nextInt();
+                    for(String line : myID.getLines(result))
+                    {
+                        System.out.println(line);
+                    }
+                    
+                    break;
+                case 5:
+                    break;
             }
-            catch(IOException e)
+            if(result == 5)
             {
-                System.out.println("IO Exception ocurred " + e);
+                break;
             }
-        }   
+            
+        }
+        sUser.close();
     }
 }
 
