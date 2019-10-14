f2f7dfe588d0bea5e30dc4ee075f5a3b2e15ae04
tommygod3 <tommygod1@live.com>
2019-10-14 14:03:20 +0000

    Added Java lab 1

--- SCC/JavaIntro/Prac1/src/myID.java
+++ SCC/JavaIntro/Prac1/src/myID.java
@@ -7,26 +7,77 @@ import java.io.*;
 
 public class myID
 {
-   public static void main(String[] args)
-   {
-      String name = "";
-      InputStreamReader isr = null;
-      BufferedReader br = null;
-      //create streams to process user input
-      isr = new InputStreamReader(System.in);
-      br = new BufferedReader(isr);
-      try {
-        //prompt user to enter name
-        System.out.println("Please Enter your name");
-        //read in the user name from the command prompt
-        name = br.readLine();
-      } 
-      catch(IOException e) {
-        System.out.println("IO Exception ocuured " + e);
-      }
-      System.out.println("Congratulations '" + name +
-                          "', you've just ran your 1st Java program.\n");
+    public String name;
+    public String email;
+    public String module;
 
-   }
+    public myID()
+    {
+        this.setDetails();
+    }
+
+    public void setDetails()
+    {
+        this.name = myID.getInputString("Please enter your name: ");
+        this.email = myID.getInputString("Please enter your email: ");
+        this.module = myID.getInputString("Please enter your module: ");
+    }
+
+    private static String getInputString(String prompt)
+    {
+        String result = "";
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        try
+        {
+            System.out.println(prompt);
+            result = br.readLine();
+        } 
+        catch(IOException e)
+        {
+            System.out.println("IO Exception ocurred " + e);
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        myID id = new myID();
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        int result;
+
+        while(true)
+        {
+            System.out.println("\nSelect from the following options:\n[1] print Name\n[2] print Email\n[3] print Module\n[4] quit program");
+            try
+            {
+                result = Integer.parseInt(br.readLine());
+                switch(result)
+                {
+                    case 1:
+                        System.out.println(id.name);
+                        break;
+                    case 2:
+                        System.out.println(id.email);
+                        break;
+                    case 3:
+                        System.out.println(id.module);
+                        break;
+                    case 4:
+                        break;
+                }
+                if(result == 4)
+                {
+                    break;
+                }
+            }
+            catch(IOException e)
+            {
+                System.out.println("IO Exception ocurred " + e);
+            }
+        }   
+    }
 }
 
