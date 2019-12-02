da5df3aa8583765b1029254a4e94ad98b7644c16
tommygod3 <tommygod1@live.com>
2019-12-02 11:48:29 +0000

    ai: Test image of shirt on load dataset

--- ai/lab7/load.py
+++ ai/lab7/load.py
@@ -1,35 +1,36 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, models, backend, layers
+import os
 
-# Load test data
-fashion_mnist = datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+import urllib.request
+import cv2
+import tensorflow as tf
+from tensorflow.keras import models, backend, layers
+import numpy
 
 # Manually set class names
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# Manually scale image data between 0-1
-train_images = train_images / 255
-test_images = test_images / 255
+urllib.request.urlretrieve("https://www.cdn-docs-cft.com/docs/img/gross/3348/michael-schumacher-t-shirt-racing.jpg", "shirt.jpg")
 
-# Add fourth dimension (channels) to data 
-# Channels = 1 in this dataset as greyscale
-img_rows = train_images.shape[1]
-img_cols = train_images.shape[2]
+img_rows = 28
+img_cols = 28
+
+shirt = cv2.imread("shirt.jpg")
+
+gray_shirt = cv2.cvtColor(shirt, cv2.COLOR_BGR2GRAY)
+scaled_shirt = cv2.resize(gray_shirt, dsize=(img_rows, img_cols), interpolation=cv2.INTER_CUBIC)
+
 if backend.image_data_format() == "channels_first":
-    train_images = train_images.reshape(train_images.shape[0], 1, img_rows, img_cols)
-    test_images = test_images.reshape(test_images.shape[0], 1, img_rows, img_cols)
+    test_images = scaled_shirt.reshape(1, 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    train_images = train_images.reshape(train_images.shape[0], img_rows, img_cols, 1)
-    test_images = test_images.reshape(test_images.shape[0], img_rows, img_cols, 1)
+    test_images = scaled_shirt.reshape(1, img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
 # Load saved model
 loaded_model = models.load_model("convolutional.h5")
 loaded_model.summary()
 
-test_loss, test_acc = loaded_model.evaluate(test_images,  test_labels, verbose=2)
+predictions = loaded_model.predict(test_images)
 
-print('\nTest accuracy:', test_acc)
+print('\n Predictions:', class_names[numpy.argmax(predictions)])
